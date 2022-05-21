ed981c78fa18d4b9413044f2fa73ad760b744ed6
EngiSon <lleopardchaun@gmail.com>
2022-05-21 15:06:16 +0200

    Initial setup
    Made files for the different components, classes and services that will be used in the future by the app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EditComponent } from './edit/edit.component';
+import { DashComponent } from './dash/dash.component';
+import { DetailComponent } from './detail/detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EditComponent,
+    DashComponent,
+    DetailComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot([
+      {path: 'todos', component: DashComponent},
+      {path: 'edit', component: EditComponent},
+      {path: 'create', component: EditComponent}
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
